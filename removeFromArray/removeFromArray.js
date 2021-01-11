f27de7ca748c51b6921abeb0cd803a14e036a838
cleunggit <cleungdev@gmail.com>
2021-01-11 15:17:04 -0500

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arg) {
+    // let args = Array.from(arg)
+    // loop through the array
+    return arr.filter(el => !arg.includes(el))
+    // compare each element to the argument
+    // if the element is not equal to the argument return the element
+    // return a new array
 }
 
 module.exports = removeFromArray
