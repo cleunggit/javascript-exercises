2138dd3074a4d6f741e0f8811031820c667a68bb
cleunggit <cleungdev@gmail.com>
2021-01-11 14:59:58 -0500

    completed first 3 exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let repeatedStr = ''
+    // if times is -1 return an error message
+    if (times < 0) return 'ERROR'
+    for (let i=0; i < times; i++) {
+        repeatedStr += word
+    }
+    return repeatedStr
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // create a variable to store the new string
+    const newStr = []
+    // convert the string into an array
+    const strArr = str.split('')
+    // loop through the array in reverse order
+    for (let i=str.length; i >= 0; i--) {
+        // add each value to the new string array
+        newStr.push(str[i])
+    }
+    // convert the array back into a string and return it
+    return newStr.join('')
 }
 
 module.exports = reverseString
